838f84c0d9cfecd0a6018fe07e68a8c01abcf499
Alejandro <al.campos@estudiantec.cr>
2024-03-17 22:50:07 -0600

    Patron de Repositorio

---
+++ SistemaInventa.AccesoDatos/Repositorio/IRepositorio/Irepositorio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventa.AccesoDatos.Repositorio.IRepositorio
+{ 
+    public interface Irepositorio<T> where T : class
+    {
+
+        Task<T>  Obtener(int id);
+        Task<IEnumerable<T>> ObtenerTodos(Expression<Func<T,bool>> filtro = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            String incluirPropiedades = null,
+            bool isTracking = true);
+        Task<T> ObtenerPrimero(Expression<Func<T, bool>> filtro = null,
+            String incluirPropiedades = null,
+            bool isTracking = true);
+        Task Agregar(T entidad);
+        void Remover(T entidad);
+
+        void RemoverRango(IEnumerable<T> entidad);
+    }
+
+       
+         
+}
